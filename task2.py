8121e2e20acf8cfcc10b99d2dbe9d5904b30306d
rinfiyks <tom.gould.92@gmail.com>
2014-05-11 00:10:34 +0100

    Correctly implement task 2

--- task2.py
+++ task2.py
@@ -4,6 +4,7 @@
 import sys
 import csv
 import random
+import math
 import numpy
 import scipy.optimize
 
@@ -17,7 +18,7 @@ def linear(InputFileName):
 
     initial_theta = [0] * len(row_data[0][-1])
 
-    return regression(row_data, initial_theta, calculate_mean_squared_error)
+    return regression(row_data, initial_theta, calculate_mean_squared_error)[0]
 
 
 def logistic(InputFileName):
@@ -25,7 +26,20 @@ def logistic(InputFileName):
     row_data = collate_row_data_logistic(raw_data)
     initial_theta = [0] * (len(row_data[0][-1]) * number_of_classes)
 
-    return regression(row_data, initial_theta, calculate_mean_classification_error)
+    theta = regression(row_data, initial_theta, calculate_mean_classification_error)[2]
+
+    correct = 0
+    incorrect = 0
+    for row in row_data:
+        predicted_class = predict_class(row, theta)
+        if predicted_class == row[2]:
+            correct += 1
+        else:
+            incorrect += 1
+
+    print("Correct: %d" % correct)
+    print("Incorrect: %d" % incorrect)
+    return 0
 
 
 def regression(row_data, initial_theta, error_function):
@@ -33,12 +47,14 @@ def regression(row_data, initial_theta, error_function):
     data_set_0 = row_data[0::2]
     data_set_1 = row_data[1::2]
 
-    error1 = train_data(data_set_0, data_set_1, initial_theta, error_function)
-    error2 = train_data(data_set_1, data_set_0, initial_theta, error_function)
+    result1 = train_data(data_set_0, data_set_1, initial_theta, error_function)
+    result2 = train_data(data_set_1, data_set_0, initial_theta, error_function)
+    error1 = result1[0]
+    error2 = result2[0]
     average_error = ((error1 + error2) / 2)
 
     print("Average error: %f" % average_error)
-    return average_error
+    return (average_error, result1[1], result2[1])
 
 
 def train_data(training_data, testing_data, theta, error_function):
@@ -46,7 +62,7 @@ def train_data(training_data, testing_data, theta, error_function):
     error = error_function(theta, testing_data)
     print(theta)
     print("error: %f" % error)
-    return error
+    return (error, theta)
 
 
 def calculate_mean_squared_error(theta, row_data):
@@ -60,33 +76,49 @@ def calculate_mean_squared_error(theta, row_data):
 
 
 def calculate_mean_classification_error(theta, row_data):
-    total_error = 0
+    total_sum = 0
 
     for row in row_data:
-        # p(y = i|X) = e^(XT_i) / sum_j(e^(XT_j))
-        # y = today's change
-        # i = row[2] = class
-        # X = row[-1] = features
-        # T = theta
+        features = row[-1]
+        features_size = len(features)
+        current_class = row[2]
 
-        # c + log(sum_i(e^(x_i - c)))
-        features_size = len(row[-1])
+        max_feature = max(features)
+        logsumexp_term = 0
+        for i in range(0, number_of_classes):
+            logsumexp_term += math.exp(numpy.dot(features, get_theta_for_class(theta, i, features_size)) - max_feature)
+        logsumexp_term = math.log(logsumexp_term)
 
-        class_theta = theta[row[2] * features_size:(row[2] + 1) * features_size]  # Theta_c
+        X_Theta_c_term = numpy.dot(features, get_theta_for_class(theta, current_class, features_size))
 
-        max_feature = max(row[-1])
-        thingy = []
-        for i in range(0, features_size):
-            thingy.append(numpy.dot([j - max_feature for j in row[-1]], theta[i * features_size:(i + 1) * features_size]))
+        total_sum += max_feature + logsumexp_term - X_Theta_c_term
 
-        total_error += numpy.dot(class_theta, row[-1])
-        - max_feature - scipy.misc.logsumexp(thingy)
-        # - scipy.misc.logsumexp(numpy.dot(theta, row[-1] * number_of_classes))
+    print total_sum
+    return total_sum
 
-        # classification = classify_day(numpy.dot(theta, row[-1] * 5), row[1])
 
-    print -total_error
-    return -total_error
+def predict_class(row, theta):
+    max = 0
+    max_class = -1
+    probabilities = []
+    for c in range(0, number_of_classes):
+        numerator = math.exp(numpy.dot(row[-1], get_theta_for_class(theta, c, len(row[-1]))))
+        denominator = 0
+        for i in range(0, number_of_classes):
+            denominator += math.exp(numpy.dot(row[-1], get_theta_for_class(theta, i, len(row[-1]))))
+        result = numerator / denominator
+        probabilities.append(result)
+        if result > max:
+            max = result
+            max_class = c
+    print("sum: %f" % sum(probabilities))
+    return max_class
+
+
+def get_theta_for_class(theta, c, features_size):
+    start_index = features_size * c
+    end_index = features_size * (c + 1)
+    return theta[start_index:end_index]
 
 
 def classify_day(today, yesterday):
@@ -110,7 +142,7 @@ def classify_day(today, yesterday):
 def get_features(rows):
     features = []
     features += [float(rows[-1][1])]  # yesterday's price
-    #  features += [1]  # constant
+    # features += [1]  # constant
     return features
 
 
