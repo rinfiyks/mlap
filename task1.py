0a4e643228cd0f4230ea4206aebe52c886bad0fc
rinfiyks <tom.gould.92@gmail.com>
2014-05-10 14:57:52 +0100

    Fix errors in 2FCV, use best features

--- task1.py
+++ task1.py
@@ -3,29 +3,37 @@
 
 import sys
 import csv
+import random
 import numpy
 import scipy.optimize
 
-previous_days_count = 10  # the number of previous days given
+previous_days_count = 1  # the number of previous days given
 
 
 def linear(InputFileName):
     raw_data = list(csv.reader(open(InputFileName, 'rU')))
-
     row_data = collate_row_data(raw_data)
 
     initial_theta = [0] * len(row_data[0][1])
 
-    training_row_data = row_data[0::2]
-    testing_row_data = row_data[1::2]
+    random.shuffle(row_data)
+    data_set_0 = row_data[0::2]
+    data_set_1 = row_data[1::2]
 
-    theta = scipy.optimize.fmin(calculate_mean_squared_error, x0=initial_theta,
-                                args=tuple([training_row_data]))
-    print("Theta:")
+    MSE1 = train_data(data_set_0, data_set_1, initial_theta)
+    MSE2 = train_data(data_set_1, data_set_0, initial_theta)
+    average_MSE = ((MSE1 + MSE2) / 2)
+
+    print("Average MSE: %f" % average_MSE)
+    return average_MSE
+
+
+def train_data(training_data, testing_data, theta):
+    theta = scipy.optimize.fmin(calculate_mean_squared_error, x0=theta, args=tuple([training_data]), disp=False)
+    MSE = calculate_mean_squared_error(theta, testing_data)
     print(theta)
-    print("Testing data MSE:")
-    calculate_mean_squared_error(theta, testing_row_data)
-    return 0
+    print("MSE: %f" % MSE)
+    return MSE
 
 
 def calculate_mean_squared_error(theta, row_data):
@@ -35,24 +43,19 @@ def calculate_mean_squared_error(theta, row_data):
         total_squared_loss += (numpy.dot(theta, row[1]) - row[0])**2
 
     total_squared_loss /= len(row_data)
-
-    print(total_squared_loss)
     return total_squared_loss
 
 
 def collate_row_data(raw_data):
     row_data = []
 
-    for i in xrange(10, len(raw_data)):
+    for i in xrange(previous_days_count, len(raw_data)):
         row_data.append([])
         row_data[-1].append(float(raw_data[i][1]))  # append price
 
         features = []
 
-        for j in range(0, 10):
-            features += [float(raw_data[i - j - 1][1])]  # add 10 days of row data
-
-        features += [1]  # constant feature
+        features += [float(raw_data[i - 1][1])]  # yesterday's price
 
         row_data[-1].append(features)
 
